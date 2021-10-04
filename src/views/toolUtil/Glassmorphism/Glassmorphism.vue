3989ccffa4b0a9aef9860a1dd63b8f171065296a
chenzhilong <97491530@qq.com>
2021-10-04 19:27:29 +0800

    添加玻璃

--- src/views/toolUtil/Glassmorphism/Glassmorphism.vue
+++ src/views/toolUtil/Glassmorphism/Glassmorphism.vue
@@ -7,15 +7,16 @@
       </div>
     </div>
     <div class="Glassmorphism-box">
-      <a-row>
+      <a-row class="Glass-row">
         <a-col :xs="24" :lg="12">
           <div class="Glass-set">
-
+            <h1 style="padding-left: 5px">Options</h1>
+            <a-slider @change="changeData" :min="0" :max="100"/>
           </div>
         </a-col>
         <a-col :xs="24" :lg="12">
-          <div class="Glass-set">
-
+          <div class="Glass-set" style="padding-top: 80px">
+            <v-md-preview :text="glassText"></v-md-preview>
           </div>
         </a-col>
       </a-row>
@@ -24,6 +25,26 @@
 </template>
 
 <script setup lang="ts">
+import {ref} from 'vue'
+
+const glassText = ref<string>('``` css\n' +
+    '\n' +
+    'filter: blur(0px);\n' +
+    '\n' +
+    '```\n')
+
+
+const changeData = (val: number): void => {
+
+  glassText.value = '``` css\n' +
+      '\n' +
+      'filter: blur('+val+'px);\n' +
+      '\n' +
+      '```\n'
+  glassStyle.value = `blur(${val}px)`
+}
+
+const glassStyle = ref<string>('blur(0px)')
 
 </script>
 <style scoped lang="scss">
@@ -34,7 +55,7 @@
 .Glassmorphism-img {
   width: 900px;
   height: 500px;
-  filter: blur(0px);
+  filter: v-bind(glassStyle);
 }
 
 .Glassmorphism-box {
@@ -54,9 +75,13 @@
   height: 300px;
   width: 440px;
   margin: 0 10px;
+  padding: 120px 10px 0 10px;
 }
 
 @media screen and (max-width: 991.98px) {
+  .Glass-row {
+    width: 100%;
+  }
   .Glassmorphism-box {
     border-radius: 8px;
   }
@@ -71,7 +96,8 @@
     box-shadow: none;
   }
   .Glass-set {
-    margin-top: 10px;
+    width: 100%;
+    margin: 10px 0 0 0;
   }
 }
 </style>
